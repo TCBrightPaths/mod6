dfd1fddb6fb29810ba08a13af4efccabd76306ba
Tamara Chance <tamara.n.chance@gmail.com>
2021-11-21 12:35:56 -0600

    pretty sure I'm not meant to fix these errors

--- functions.test.ts
+++ functions.test.ts
@@ -2,12 +2,13 @@ const { shuffleArray } = require('./utils')
 
 describe('shuffleArray should', () => {
 
-    test('return Array', () => {
+    test('return array', () => {
         let shuffledArray = shuffleArray()
         expect(typeof shuffledArray).toBe('array')
     })
     test('returns array with same length as argument', () => {
-        let shuffledArray = shuffleArray()
+        let array = [1,2,3,4,5]
+        let shuffledArray = shuffleArray(array)
         expect(shuffledArray.length).toEqual(array.length)
     })
 })
--- utils.js
+++ utils.js
@@ -1,14 +1,14 @@
 function shuffleArray(array) {
-    let arrCopy = [...array]
+    let arrCopy = array //-->maybe fixed this so that it would just be one array instead of an object inside of an array. 
     for (let i = arrCopy.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [arrCopy[i], arrCopy[j]] = [arrCopy[j], arrCopy[i]];
     }
     return arrCopy
-    //console.log(typeof arrCopy) -->WOW it was never actually an array?
+    //console.log(arrCopy.length) //-->WOW it was never actually an array? & the function adds an array into another array? so it always has a count of one? So were my tests supposed to fail?!!!!!!! 
 }
 
-//console.log(shuffleArray([1,2,3,4,5]))
+//(shuffleArray([1,2,3,4,5])) //--> function returns an array of numbers correctly shuffled. Everything should work but somehow doesn't when I run my tests. 
 
 module.exports = {
     shuffleArray
