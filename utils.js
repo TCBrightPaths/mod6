9b06c88ba5559117266f62eea49797bdb7a99b6d
Tamara Chance <tamara.n.chance@gmail.com>
2021-11-21 12:14:03 -0600

    not actually an array and yeah that took so long for me to figure out

--- utils.js
+++ utils.js
@@ -1,11 +1,14 @@
 function shuffleArray(array) {
-    let arrCopy = [array]
+    let arrCopy = [...array]
     for (let i = arrCopy.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [arrCopy[i], arrCopy[j]] = [arrCopy[j], arrCopy[i]];
     }
     return arrCopy
+    //console.log(typeof arrCopy) -->WOW it was never actually an array?
 }
+
+//console.log(shuffleArray([1,2,3,4,5]))
 
 module.exports = {
     shuffleArray
