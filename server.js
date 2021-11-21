0f88498413f671ed3087685f527de8b3b00db8d0
Tamara Chance <tamara.n.chance@gmail.com>
2021-11-20 19:31:21 -0600

    correcting cannot find file error

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 app.use(express.static('public')) //Added this to bring in the docs in the public folder.
 
-//Trying to fix promise issue.
+//To fix promise issue. 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.header(
